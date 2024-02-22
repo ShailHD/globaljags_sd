12f3dbd76fdc3eacec22f31a50ce89fe8bcdff54
Shail Desai <shaidesa@iu.edu>
2024-02-22 23:43:27 +0000

    Code changes to v4

--- index.js
+++ index.js
@@ -1,19 +1,43 @@
-// Updated index.js
-
 // Imports
 const fs = require('fs');
+const { Storage } = require('@google-cloud/storage');
+const { Firestore } = require('@google-cloud/firestore');
 const getExif = require('exif-async');
 const parseDMS = require('parse-dms');
 
-// Entry Point Function
-async function extractExif() {
+// This code will initialize Google Cloud clients
+const storage = new Storage();
+const firestore = new Firestore();
+
+// These are the Bucket Details
+const thumbnailBucketName = 'sp24-41200-shail-globaljags-thumbnails';
+const imageBucketName = 'sp24-41200-shail-globaljags-uploads';
+
+// This is Entry Point Function
+async function extractExif(file, context) {
     console.log("Script started");
+    const filePath = file.name;
+    const fileName = filePath.split('/').pop();
+
     try {
-        let gpsObject = await readExifData('./china1.jpeg'); // Ensure the path to the image is correct
+        let gpsObject = await readExifData(`./${fileName}`); // Ensure the path to the image is correct
         if (gpsObject) {
-            console.log(gpsObject);
             let gpsDecimal = getGPSCoordinates(gpsObject);
-            console.log('Decimal Coordinates:', gpsDecimal);
+            const thumbnailUrl = `https://storage.googleapis.com/${thumbnailBucketName}/${fileName}`;
+            const imageUrl = `https://storage.googleapis.com/${imageBucketName}/${filePath}`;
+
+            // This code will construct the document to be written to Firestore
+            const photoData = {
+                thumbnailUrl,
+                imageUrl,
+                latitude: gpsDecimal.latitude,
+                longitude: gpsDecimal.longitude,
+                imageName: fileName
+            };
+
+            // This code will write to Firestore
+            await firestore.collection('photos').doc(fileName).set(photoData);
+            console.log('Document written to Firestore:', photoData);
         } else {
             console.log("No GPS data found or unable to read EXIF.");
         }
@@ -22,12 +46,12 @@ async function extractExif() {
     }
 }
 
-// Helper Functions
+// These are the helper functions
 async function readExifData(localFile) {
     try {
         const exifData = await getExif(localFile);
         return exifData.gps;
-    } catch(err) {
+    } catch (err) {
         console.error('Error reading EXIF data:', err);
         return null;
     }
@@ -42,8 +66,10 @@ function getGPSCoordinates(gpsData) {
     const lonString = `${gpsData.GPSLongitude[0]}:${gpsData.GPSLongitude[1]}:${gpsData.GPSLongitude[2]}${gpsData.GPSLongitudeRef}`;
 
     const degCoords = parseDMS(`${latString} ${lonString}`);
-    return degCoords;
+    return {
+        latitude: degCoords.lat,
+        longitude: degCoords.lon
+    };
 }
 
-// Start the process
-extractExif();
+exports.extractExif = extractExif; // Export the function
